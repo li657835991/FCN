197ef5d54b41fe465fc76868542beec5c82bf5ee
li657835991 <657835991@qq.com>
2018-04-25 01:23:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup script."""
+
+import setuptools
+
+setuptools.setup(
+    author="Jihong Ju",
+    author_email="daniel.jihong.ju@gmail.com",
+    extras_require={
+        "test": [
+            'keras>=2.0.0',
+            "pandas==0.19.2",
+            "tensorflow",
+            "codecov",
+            "mock",
+            "pytest",
+            "pytest-cov",
+            "pytest-pep8",
+            "pytest-runner",
+            "pydot",
+            "graphviz"
+        ],
+    },
+    install_requires=[],
+    name="keras-fcn",
+    description=("A reimplemtenation of fully convolutional networks"),
+    packages=['keras_fcn'],
+    url="https://github.com/JihongJu/keras-fcn",
+    version="0.0.1",
+)
---
+++ show_nets.py
@@ -0,0 +1,13 @@
+from keras.utils import plot_model
+from keras_fcn import FCN_VGG16
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+def vis_fcn_vgg16():
+    input_shape = (500, 500, 3)
+    fcn_vgg16 = FCN_VGG16(input_shape=input_shape, classes=21)
+    plot_model(fcn_vgg16, to_file='fcn_vgg16.png')
+
+
+if __name__ == "__main__":
+    vis_fcn_vgg16()
